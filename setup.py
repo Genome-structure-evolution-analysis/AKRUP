08f5b3a2d7c4cc2375245bce4d7916a22e1e19da
wangjiaqi206 <wangjiaqiky@163.com>
2023-08-12 05:45:23 +0800

    V1.0.2

--- setup.py
+++ setup.py
@@ -8,15 +8,17 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setup(
     name='AKRUP',
-    version='1.0.1',
+    version='1.0.2',
     author="wangjiaqi",
     description="Ancestral Karyotype Reconstruction Universal Pipeline",
+    url="https://github.com/Genome-structure-evolution-analysis/AKRUP",
     packages=find_packages(),
     include_package_data=True,
     license="BSD License",
     long_description=long_description,
     long_description_content_type="text/markdown",
     install_requires=required,
+    # package_data={'': ['*.conf', '*.pl', '*.pm','*.R', '*.exe'], 'ini': ['*']},
     package_data={'': ['*.conf', '*']},
     entry_points={
         'console_scripts': [
